b3e2c3f462c17530c67c706dc0a57733ad9ff03d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-27 18:48:17 -0400

    add workouts_index view

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .models import Workout
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def workouts_index(request):
+    workouts = Workout.objects.all()
+    return render(request, "workouts/index.html", {"workouts": workouts})
