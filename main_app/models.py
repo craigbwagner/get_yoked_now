6c70deea931e0f0e927e4c6bfdef2bb065fde715
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-27 19:19:24 -0400

    add date and notes field to Workout model

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Workout(models.Model):
     split = models.CharField(max_length=50)
+    date = models.DateField("Workout Date")
+    notes = models.TextField(max_length=300)
 
     def __str__(self):
         return self.split
